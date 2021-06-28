7abe06fdd1812899bf0349c4f1fd4e1a33b9a58d
Kutsak-Nataly <73638937+Kutsak-Nataly@users.noreply.github.com>
2021-06-28 09:03:57 +0300

    refactor: validate

--- src/authentication/validate-session.ts
+++ src/authentication/validate-session.ts
@@ -2,27 +2,20 @@ import {NextFunction, Request, Response} from 'express';
 import jwt from 'jsonwebtoken';
 import {JWT_SECRET_KEY} from '../common/config'
 import {usersService} from '../resources/users/user.service';
+import {getToken} from './utilities';
 
 const validation = (req: Request, res: Response, next: NextFunction): void => {
-    if (req.method === 'OPTIONS') {
-        next();   // allowing options as a method for request
-    } else {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) res.status(401).json({auth: false, message: "No token provided"});
-        else {
-            const token = authHeader.split(' ')[1];
-            if (!token) res.status(401).json({auth: false, message: "No token provided"});
-            else {
-                jwt.verify(token, JWT_SECRET_KEY, (_err, decoded) => {
-                    if (decoded) {
-                        usersService.getById(decoded['id']).then().catch(err => next(err));
-                        next();
-                    } else {
-                        res.status(408).send({error: "not authorized"});
-                    }
-                });
+    const token = getToken(req.headers.authorization);
+    if (!token) res.status(401).json({auth: false, message: "No token provided"});
+    else {
+        jwt.verify(token, JWT_SECRET_KEY, (_err, decoded) => {
+            if (decoded) {
+                usersService.getById(decoded['userId']).then().catch(err => next(err));
+                next();
+            } else {
+                res.status(408).send({error: "not authorized"});
             }
-        }
+        });
     }
 };
 
